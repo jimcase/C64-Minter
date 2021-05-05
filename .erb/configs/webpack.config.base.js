0a49a635d4f83cc1f93137a131d35383cb4af903
jimcase <jcaso@protonmail.com>
2021-05-05 23:30:12 +0100

    feat: migrate repo 2

---
+++ .erb/configs/webpack.config.base.js
@@ -0,0 +1,54 @@
+/**
+ * Base webpack config used across other specific configs
+ */
+
+import path from 'path';
+import webpack from 'webpack';
+import { dependencies as externals } from '../../../../C64-Minter2/src/package.json';
+
+export default {
+  externals: [...Object.keys(externals || {})],
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, '../../src'),
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: 'commonjs2',
+  },
+
+  /**
+   * Determine the array of extensions that should be used to resolve modules.
+   */
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+    modules: [path.join(__dirname, '../../src'), 'node_modules'],
+  },
+
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+
+  /**
+   * WASM config, for cardano-serialization-lib
+   */
+  experiments: {
+    // asyncWebAssembly: true,
+    syncWebAssembly: true,
+  },
+};
